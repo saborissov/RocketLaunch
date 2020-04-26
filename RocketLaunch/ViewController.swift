5a550b976df0291862712f19db51981da7c327d0
Sergey Borisov <it.saborisov@gmail.com>
2020-04-26 10:41:46 +0300

    addCommit

--- RocketLaunch/ViewController.swift
+++ RocketLaunch/ViewController.swift
@@ -7,14 +7,35 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
+    @IBOutlet weak var rocketContentView: UIView!
+    @IBOutlet weak var rocketImageView: UIImageView!
+    @IBOutlet weak var successLabel: UILabel!
+    
+    var player: AVAudioPlayer!
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        let path = Bundle.main.path(forResource: "launch", ofType: "mp3")
+        let url = URL(fileURLWithPath: path!)
+        player = try? AVAudioPlayer(contentsOf: url)
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func powerButtonPress(_ sender: UIButton) {
+        rocketContentView.isHidden = false
+        player.play()
+        successLabel .isHidden=true
+        let frame = rocketImageView.frame
+        rocketImageView.frame.origin.y = 1000
+        UIView.animate(withDuration: 3.5, animations: {
+            self.rocketImageView.frame = frame
+        }) { (_) in
+            self.successLabel.isHidden=false
+        }
+    }
+    
 }
 
